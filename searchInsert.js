8477fbccbae717b8ffead14467a0005f5fe6bd6f
XandraV <veresalexandra95@gmail.com>
2023-10-12 09:09:47 +0100

    binary search

---
+++ searchInsert.js
@@ -0,0 +1,23 @@
+//Given a sorted array of distinct integers and a target value,
+//return the index if the target is found.
+// If not, return the index where it would be
+//if it were inserted in order.
+
+//You must write an algorithm with O(log n) runtime complexity.
+// Binary search
+function searchInsert(nums, target) {
+  let lo = 0,
+    hi = nums.length; // we might need to insert at the end
+  while (lo < hi) {
+    // breaks if lo == hi
+    let mid = lo + Math.floor((hi - lo) / 2); // always gives the lower mid
+    if (target > nums[mid]) {
+      lo = mid + 1; // no way mid is a valid option
+    } else {
+      hi = mid; // it might be possibe to inseart @ mid
+    }
+  }
+  return lo;
+}
+
+console.log(searchInsert([1, 5, 6, 9, 14], 12));
