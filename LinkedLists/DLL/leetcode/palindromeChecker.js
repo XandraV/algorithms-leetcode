463597b755649181bb73a05bf0a894f8a8fb5a73
XandraV <veresalexandra95@gmail.com>
2025-06-04 17:44:23 +0100

    doubly linked list leetcode

---
+++ LinkedLists/DLL/leetcode/palindromeChecker.js
@@ -0,0 +1,127 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length);
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // the loop iterates through only half of the list hence complexity is O(n)
+  // space complexity is O(1) because the algorithm uses a constant amount of extra space
+  // regardless of the size of the list.
+  isPalindrome() {
+    if (this.length <= 1) return true;
+
+    let head = this.head;
+    let tail = this.tail;
+
+    // if head.value == tail.value -> head.next.value == tail.prev.value etc
+
+    for (let i = 0; i < this.length / 2; i++) {
+      if (head.value === tail.value) {
+        head = head.next;
+        tail = tail.prev;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let myDoublyLinkedList = new DoublyLinkedList(1);
+myDoublyLinkedList.push(2);
+myDoublyLinkedList.push(3);
+myDoublyLinkedList.push(2);
+myDoublyLinkedList.push(1);
+
+console.log("List 1:");
+myDoublyLinkedList.printList();
+console.log("Is List 1 a palindrome? " + myDoublyLinkedList.isPalindrome());
+
+let myDoublyLinkedList2 = new DoublyLinkedList(1);
+myDoublyLinkedList2.push(2);
+myDoublyLinkedList2.push(3);
+myDoublyLinkedList2.push(4);
+myDoublyLinkedList2.push(5);
+
+console.log("\nList 2:");
+myDoublyLinkedList2.printList();
+console.log("Is List 2 a palindrome? " + myDoublyLinkedList2.isPalindrome());
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    List 1:
+    1
+    2
+    3
+    2
+    1
+    Is List 1 a palindrome? true
+
+    List 2:
+    1
+    2
+    3
+    4
+    5
+    Is List 2 a palindrome? false
+*/
