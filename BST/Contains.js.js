6d6eb732a065f32125ac9a4cefb1a98d4fbb05c6
XandraV <veresalexandra95@gmail.com>
2025-06-24 08:22:10 +0100

    bst contains method

--- BST/Contains.js.js
+++ BST/Contains.js.js
@@ -19,18 +19,27 @@ class BST {
     }
     let temp = this.root;
     while (true) {
+      // If the value already exists, return undefined
+      // to indicate that duplicates are not allowed
       if (newNode.value === temp.value) return undefined;
+
+      // If the new value is less than the current node's value,
+      // move to the left child; otherwise, move to the right child
       if (newNode.value < temp.value) {
+        // If the left child is null, insert the new node here
         if (temp.left === null) {
           temp.left = newNode;
           return this;
         }
+        // Otherwise, move to the left child
         temp = temp.left;
       } else {
+        // If the right child is null, insert the new node here
         if (temp.right === null) {
           temp.right = newNode;
           return this;
         }
+        // Otherwise, move to the right child
         temp = temp.right;
       }
     }
@@ -42,14 +51,19 @@ class BST {
     let temp = this.root;
 
     while (temp) {
+      // If the value is less than the current node's value,
+      // move to the left child; if it's greater, move to the right child
+      // else if it matches, return true
       if (value < temp.value) {
         temp = temp.left;
-      } else if (temp > temp.value) {
+      } else if (value > temp.value) {
         temp = temp.right;
       } else {
         return true;
       }
     }
+    // if temp becomes null, the value is not found
+    // so we return false
     return false;
   }
 }
