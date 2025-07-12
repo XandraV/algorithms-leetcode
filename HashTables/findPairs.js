9ca930c0bf389b5b90edf0f6e687e88c8dc195e6
XandraV <veresalexandra95@gmail.com>
2025-07-12 15:54:44 +0100

    find pairs leetcode

---
+++ HashTables/findPairs.js
@@ -0,0 +1,61 @@
+// This function finds all the pairs of numbers that
+// sum up to a given target.
+// One number is taken from arr1 and the other from
+// arr2.
+function findPairs(arr1, arr2, target) {
+  const mySet = new Set();
+  const pairs = [];
+
+  for (const num of arr1) {
+    mySet.add(num);
+  }
+
+  for (const num of arr2) {
+    const complement = target - num;
+    if (mySet.has(complement)) {
+      pairs.push([complement, num]);
+    }
+  }
+
+  return pairs;
+}
+
+// -------------------
+// Single Pair Matching
+// -------------------
+console.log("Single Pair Matching:");
+console.log("Input: [1, 2, 3], [4, 5, 6], 7");
+console.log("Output: ", findPairs([1, 2, 3], [4, 5, 6], 7));
+console.log("---------------");
+
+// -------------------
+// Multiple Pairs
+// -------------------
+console.log("Multiple Pairs:");
+console.log("Input: [1, 2, 3], [7, 6, 5], 8");
+console.log("Output: ", findPairs([1, 2, 3], [7, 6, 5], 8));
+console.log("---------------");
+
+// -------------------
+// No Matching Pairs
+// -------------------
+console.log("No Matching Pairs:");
+console.log("Input: [1, 2, 3], [7, 8, 9], 5");
+console.log("Output: ", findPairs([1, 2, 3], [7, 8, 9], 5));
+console.log("---------------");
+
+// -------------------
+// Empty Arrays
+// -------------------
+console.log("Empty Arrays:");
+console.log("Input: [], [], 10");
+console.log("Output: ", findPairs([], [], 10));
+console.log("---------------");
+
+// -------------------
+// One Empty Array
+// -------------------
+console.log("One Empty Array:");
+console.log("Input: [1, 2, 3], [], 4");
+console.log("Output: ", findPairs([1, 2, 3], [], 4));
+console.log("---------------");
