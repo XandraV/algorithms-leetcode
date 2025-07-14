058928fbabbdd299554bf85b82955ef06a67d48d
XandraV <veresalexandra95@gmail.com>
2025-07-14 20:06:26 +0100

    remove duplicates

--- HashTables/removeDuplicates.js
+++ HashTables/removeDuplicates.js
@@ -12,41 +12,30 @@ function removeDuplicates2(arr) {
 
   return Array.from(myMap.keys());
 }
-// ---------------
-// No Duplicates
-// ---------------
+
 console.log("No Duplicates:");
 console.log("Input: [1, 2, 3]");
 console.log("Output: ", removeDuplicates([1, 2, 3]));
 console.log("---------------");
 
-// ---------------
-// With Duplicates
-// ---------------
+
 console.log("With Duplicates:");
 console.log("Input: [1, 2, 2, 3, 3, 3]");
 console.log("Output: ", removeDuplicates([1, 2, 2, 3, 3, 3]));
 console.log("---------------");
 
-// ---------------
-// Mixed Types
-// ---------------
+
 console.log("Mixed Types:");
 console.log('Input: [1, "1", true, "true"]');
 console.log("Output: ", removeDuplicates([1, "1", true, "true"]));
 console.log("---------------");
 
-// ---------------
-// Empty Array
-// ---------------
+
 console.log("Empty Array:");
 console.log("Input: []");
 console.log("Output: ", removeDuplicates([]));
 console.log("---------------");
 
-// ---------------
-// Single Element
-// ---------------
 console.log("Single Element:");
 console.log("Input: [1]");
 console.log("Output: ", removeDuplicates([1]));
