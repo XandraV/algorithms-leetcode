5a0e64d978671ccf10fd31d47c5fce7dd984189c
XandraV <veresalexandra95@gmail.com>
2025-07-15 16:01:13 +0100

    leetcode cleanup

--- HashTables/longestConsecutiveSequence.js
+++ HashTables/longestConsecutiveSequence.js
@@ -1,7 +1,6 @@
-
-// This function finds the length of the longest 
-// consecutive sequence of integers in the given 
-// array.                                        
+// This function finds the length of the longest
+// consecutive sequence of integers in the given
+// array.
 function longestConsecutiveSequence(input) {
   if (input.length === 0) return 0;
 
@@ -25,72 +24,55 @@ function longestConsecutiveSequence(input) {
 }
 
 function longestConsecutiveSequence2(nums) {
-    const numSet = new Set();
-    for (const num of nums) {
-        numSet.add(num);
+  const numSet = new Set();
+  for (const num of nums) {
+    numSet.add(num);
+  }
+
+  let longestStreak = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longestStreak = Math.max(longestStreak, currentStreak);
     }
- 
-    let longestStreak = 0;
- 
-    for (const num of numSet) {
-        if (!numSet.has(num - 1)) {
-            let currentNum = num;
-            let currentStreak = 1;
- 
-            while (numSet.has(currentNum + 1)) {
-                currentNum++;
-                currentStreak++;
-            }
- 
-            longestStreak = Math.max(longestStreak, currentStreak);
-        }
-    }
- 
-    return longestStreak;
+  }
+
+  return longestStreak;
 }
-// -------------------
-// No Consecutive Sequence
-// -------------------
+
 console.log("No Consecutive Sequence:");
 console.log("Input: [1, 3, 5]");
 console.log("Output: ", longestConsecutiveSequence([1, 3, 5]));
 console.log("---------------");
 
-// -------------------
-// Single Element
-// -------------------
 console.log("Single Element:");
 console.log("Input: [1]");
 console.log("Output: ", longestConsecutiveSequence([1]));
 console.log("---------------");
 
-// -------------------
-// Consecutive Sequence
-// -------------------
 console.log("Consecutive Sequence:");
 console.log("Input: [1, 2, 3, 4, 5]");
 console.log("Output: ", longestConsecutiveSequence([1, 2, 3, 4, 5]));
 console.log("---------------");
 
-// -------------------
-// Unordered Input
-// -------------------
 console.log("Unordered Input:");
 console.log("Input: [5, 2, 3, 1, 4]");
 console.log("Output: ", longestConsecutiveSequence([5, 2, 3, 1, 4]));
 console.log("---------------");
 
-// -------------------
-// Empty Array
-// -------------------
 console.log("Empty Array:");
 console.log("Input: []");
 console.log("Output: ", longestConsecutiveSequence([]));
 console.log("---------------");
 
-// -------------------
-// Multiple Sequences
-// -------------------
 console.log("Multiple Sequences:");
 console.log("Input: [1, 2, 3, 10, 11, 12]");
 console.log("Output: ", longestConsecutiveSequence([1, 2, 3, 10, 11, 12]));
