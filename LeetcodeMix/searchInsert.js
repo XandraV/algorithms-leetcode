a42b5151945f44f1eece2d13b3b18dffd7c02238
XandraV <veresalexandra95@gmail.com>
2025-07-22 12:20:41 +0100

    leetcode search insert

--- LeetcodeMix/searchInsert.js
+++ LeetcodeMix/searchInsert.js
@@ -1,20 +1,19 @@
 //Given a sorted array of distinct integers and a target value,
-//return the index if the target is found.
+// return the index if the target is found.
 // If not, return the index where it would be
-//if it were inserted in order.
+// if it were inserted in order.
 
-//You must write an algorithm with O(log n) runtime complexity.
 // Binary search
 function searchInsert(nums, target) {
   let lo = 0,
     hi = nums.length; // we might need to insert at the end
   while (lo < hi) {
     // breaks if lo == hi
-    let mid = lo + Math.floor((hi - lo) / 2); // always gives the lower mid
+    let mid = lo + Math.floor((hi - lo) / 2);
     if (target > nums[mid]) {
-      lo = mid + 1; // no way mid is a valid option
+      lo = mid + 1; 
     } else {
-      hi = mid; // it might be possibe to inseart @ mid
+      hi = mid; 
     }
   }
   return lo;
