918c67a71a23d5828afbadb4a0c30b194b986251
XandraV <veresalexandra95@gmail.com>
2025-09-05 19:46:27 +0100

    heap remove method

--- Heap/heap.js
+++ Heap/heap.js
@@ -36,4 +36,43 @@ class Heap {
       current = this.#parentIndex(current);
     }
   }
+
+  #sinkDown(index) {
+    let maxIndex = index;
+    let size = this.#heap.length;
+    while (true) {
+      let leftChildIndex = this.#leftChildIndex(index);
+      let rightChildIndex = this.#rightChildIndex(index);
+
+      if (
+        leftChildIndex < size &&
+        this.#heap[leftChildIndex] > this.#heap[maxIndex]
+      ) {
+        maxIndex = leftChildIndex;
+      }
+
+      if (
+        rightChildIndex < size &&
+        this.#heap[rightChildIndex] > this.#heap[maxIndex]
+      ) {
+        maxIndex = rightChildIndex;
+      }
+
+      if (maxIndex !== index) {
+        this.#swap(index, maxIndex);
+        index = maxIndex;
+      } else {
+        return;
+      }
+    }
+  }
+
+  remove() {
+    if (this.#heap.length === 0) return null;
+    if (this.#heap.length === 1) return this.#heap.pop();
+    const maxValue = this.#heap[0];
+    this.#heap[0] = this.#heap.pop();
+    this.#sinkDown(0);
+    return maxValue;
+  }
 }
