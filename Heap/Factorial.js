c1523439d048b4c58cb005bfcd28421d0ce1b43e
XandraV <veresalexandra95@gmail.com>
2025-09-06 10:36:57 +0100

    factorial call stack

--- Heap/Factorial.js
+++ Heap/Factorial.js
@@ -1,7 +1,26 @@
 function factorial(n) {
-    if(n === 1) return 1
-    return n * factorial(n-1)
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
 }
 
+factorial(4);
+
+/*
+Call stack for factorial(4):
 
 factorial(4)
+    -> 4 * factorial(3)
+            -> 3 * factorial(2)
+                    -> 2 * factorial(1)
+                            -> returns 1
+                    <- 2 * 1 = 2
+            <- 3 * 2 = 6
+    <- 4 * 6 = 24
+
+    Call stack visualization:
+    |factorial(4)| = |1!|
+    |factorial(3)| = |2!|
+    |factorial(2)| = |3!|
+    |factorial(1)| = |4!|
+    |____________| = |__|
+*/
