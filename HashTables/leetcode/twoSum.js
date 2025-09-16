a7924d0285b67fc9f1deee82d87296a5c3edf521
XandraV <veresalexandra95@gmail.com>
2025-09-16 13:27:09 +0100

    two sum leetcode

--- HashTables/leetcode/twoSum.js
+++ HashTables/leetcode/twoSum.js
@@ -5,9 +5,12 @@ function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
     const complement = target - num;
+    // check if the complement of the current num
+    // already exists as a key in our map
     if (numMap.has(complement)) {
       return [numMap.get(complement), i];
     }
+    // store the number from nums array as keys and index as value
     numMap.set(num, i);
   }
   return [];
@@ -18,30 +21,25 @@ console.log("Input: [2, 7, 11, 15], Target: 9");
 console.log("Output: ", JSON.stringify(twoSum([2, 7, 11, 15], 9)));
 console.log("---------------");
 
-
 console.log("Duplicate Numbers:");
 console.log("Input: [3, 3, 11, 15], Target: 6");
 console.log("Output: ", JSON.stringify(twoSum([3, 3, 11, 15], 6)));
 console.log("---------------");
-
 
 console.log("No Solution:");
 console.log("Input: [2, 7, 11, 15], Target: 30");
 console.log("Output: ", JSON.stringify(twoSum([2, 7, 11, 15], 30)));
 console.log("---------------");
 
-
 console.log("Negative Numbers:");
 console.log("Input: [-1, -2, -3, -4, -5], Target: -8");
 console.log("Output: ", JSON.stringify(twoSum([-1, -2, -3, -4, -5], -8)));
 console.log("---------------");
 
-
 console.log("Empty Array:");
 console.log("Input: [], Target: 0");
 console.log("Output: ", JSON.stringify(twoSum([], 0)));
 console.log("---------------");
-
 
 // inefficient nested loop solution
 function twoSum2(nums, target) {
