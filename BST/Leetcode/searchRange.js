1195aec1b3d0fdf97b7fba21de5edf15f6ff4fb2
XandraV <veresalexandra95@gmail.com>
2025-12-26 16:01:32 +0000

    clean up

--- BST/Leetcode/searchRange.js
+++ BST/Leetcode/searchRange.js
@@ -50,44 +50,44 @@ const searchRange = function (nums, target) {
 // Test 1
 const nums1 = [5, 7, 7, 8, 8, 10];
 const target1 = 8;
-console.log(searchRange(nums1, target1)); // Expected output: [3, 4]
+console.log(searchRange(nums1, target1)); // [3, 4]
 
 // Test 2
 const nums2 = [5, 7, 7, 8, 8, 10];
 const target2 = 6;
-console.log(searchRange(nums2, target2)); // Expected output: [-1, -1]
+console.log(searchRange(nums2, target2)); // [-1, -1]
 
 // Test 3
 const nums3 = [];
 const target3 = 0;
-console.log(searchRange(nums3, target3)); // Expected output: [-1, -1]
+console.log(searchRange(nums3, target3)); // [-1, -1]
 
 // Test 4: Single element, target present
 const nums4 = [1];
 const target4 = 1;
-console.log(searchRange(nums4, target4)); // Expected output: [0, 0]
+console.log(searchRange(nums4, target4)); // [0, 0]
 
 // Test 5: Single element, target absent
 const nums5 = [1];
 const target5 = 2;
-console.log(searchRange(nums5, target5)); // Expected output: [-1, -1]
+console.log(searchRange(nums5, target5)); // [-1, -1]
 
 // Test 6: Multiple occurrences
 const nums6 = [2, 2, 2, 2, 2];
 const target6 = 2;
-console.log(searchRange(nums6, target6)); // Expected output: [0, 4]
+console.log(searchRange(nums6, target6)); // [0, 4]
 
 // Test 7: Target at the beginning
 const nums7 = [3, 3, 4, 5, 6];
 const target7 = 3;
-console.log(searchRange(nums7, target7)); // Expected output: [0, 1]
+console.log(searchRange(nums7, target7)); // [0, 1]
 
 // Test 8: Target at the end
 const nums8 = [1, 2, 3, 4, 5, 5];
 const target8 = 5;
-console.log(searchRange(nums8, target8)); // Expected output: [4, 5]
+console.log(searchRange(nums8, target8)); // [4, 5]
 
 // Test 8: Target at the end
 const nums9 = [1, 2, 3, 3, 3, 4, 4, 5, 5];
 const target9 = 3;
-console.log(searchRange(nums9, target9)); // Expected output: [2, 4]
+console.log(searchRange(nums9, target9)); // [2, 4]
