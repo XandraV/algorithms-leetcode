34e3f299a6cce0f178e94ab2da2310a946bda78a
XandraV <veresalexandra95@gmail.com>
2026-01-15 19:15:47 +0000

    clean up

--- FE/findSingleInteger.ts
+++ FE/findSingleInteger.ts
@@ -1,5 +1,4 @@
-// Given an array of integers, all integers appear twice except one integer, could you quickly target it?
-
+// Find the integer in an array of integers that appears only once.
 function findSingle(arr: number[]): number | undefined {
   let result = 0;
   for (const num of arr) {
@@ -11,21 +10,26 @@ function findSingle(arr: number[]): number | undefined {
 console.log(10 ^ 3 ^ 3);
 console.log(10 ^ 0);
 
-// 10 = 2^3 + 2^1
-// 1010
-//    0
-// ----- XOR
-// 1010
+// All paris cancel out eg.:
+// 10 ^ 10 = 0
+// 2 ^ 2 = 0
+
+// 10 ^ 2 ^ 2 ^ 1 ^ 0 ^ 0 ^ 10
+// = (10 ^ 10) ^ (2 ^ 2) ^ (0 ^ 0) ^ 1
+// = 0 ^ 0 ^ 0 ^ 1
+// = 1
 
 const arr = [10, 2, 2, 1, 0, 0, 10];
 console.log(findSingle(arr)); // 1
 
 function findSingle2(arr: number[]): number | undefined {
   for (let i = 0; i < arr.length; i++) {
-    let curr = arr[i];
+    let curr = arr[i]; // fix the curr variable
 
+    // create a new array without the current element
     let temp = arr.toSpliced(i, 1);
     if (!temp.includes(curr)) {
+      // check if the new array includes the current element
       return curr;
     }
   }
