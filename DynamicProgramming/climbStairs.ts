eabc8f9d4062dbafb13cdaa41314c168e81f3b58
XandraV <veresalexandra95@gmail.com>
2026-01-17 15:01:28 +0000

    climbing stairs

---
+++ DynamicProgramming/climbStairs.ts
@@ -0,0 +1,17 @@
+function climbStairs(n: number): number {
+  if (n <= 1) return 1;
+
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+console.log(climbStairs(2)); // 2
+console.log(climbStairs(3)); // 3
+console.log(climbStairs(4)); // 5
+console.log(climbStairs(5)); // 8
