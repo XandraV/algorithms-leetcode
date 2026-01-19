0d3dcd80d6f3d5997fd75c3936458d3ea0a6d869
XandraV <veresalexandra95@gmail.com>
2026-01-19 20:47:38 +0000

    min cost climbing stairs

---
+++ DynamicProgramming/minCostClimbingStairs.ts
@@ -0,0 +1,24 @@
+// You are given an integer array cost where cost[i] is the cost of ith
+// step on a staircase. Once you pay the cost, you can either climb one or two steps.
+// You can either start from the step with index 0, or the step with index 1.
+// Return the minimum cost to reach the top of the floor.
+function minCostClimbingStairs(cost: number[]): number {
+  if (cost.length === 0) return 0;
+  if (cost.length === 1) return 1;
+
+  const dp: number[] = new Array(cost.length + 1).fill(0);
+
+  dp[0] = cost[0];
+  dp[1] = Math.min(cost[0] + cost[1], cost[1]);
+
+  for (let i = 2; i <= cost.length; i++) {
+    let tempCost = cost[i] || 0;
+    dp[i] = Math.min(dp[i - 1] + tempCost, dp[i - 2] + tempCost);
+  }
+
+  return dp[dp.length - 1];
+}
+
+const cost = [10, 15, 20];
+console.log(minCostClimbingStairs(cost));
+// 15
