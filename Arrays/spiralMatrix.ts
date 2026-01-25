97431c74e2f78af15eaff8162c136aee791b2596
XandraV <veresalexandra95@gmail.com>
2026-01-25 10:39:29 +0000

    spiral matrix comments

--- Arrays/spiralMatrix.ts
+++ Arrays/spiralMatrix.ts
@@ -1,5 +1,9 @@
 // Given an m x n matrix, return all elements of the matrix in spiral order.
+// We shrink the rectangle boundaries after traversing each side
 function spiralOrder(matrix: number[][]): number[] {
+  // for empty input matrix
+  if (matrix.length === 0) return [];
+
   const res = [];
   let top = 0;
   let bottom = matrix.length;
@@ -7,13 +11,16 @@ function spiralOrder(matrix: number[][]): number[] {
   let right = matrix[0].length;
 
   while (left < right && top < bottom) {
+     // left → right
     for (let i = left; i < right; i++) {
       res.push(matrix[top][i]);
     }
     top++;
+
     if (top >= bottom) {
       break;
     }
+    // top → bottom
     for (let i = top; i < bottom; i++) {
       res.push(matrix[i][right - 1]);
     }
@@ -21,10 +28,13 @@ function spiralOrder(matrix: number[][]): number[] {
     if (left >= right) {
       break;
     }
+    // right → left
     for (let i = right - 1; i >= left; i--) {
       res.push(matrix[bottom - 1][i]);
     }
     bottom--;
+
+    // bottom → top
     for (let i = bottom - 1; i >= top; i--) {
       res.push(matrix[i][left]);
     }
