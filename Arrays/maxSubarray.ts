e9158c3e60be4f718710c89aa8293dd5daf80cbe
XandraV <veresalexandra95@gmail.com>
2026-01-29 21:34:23 +0000

    maximum subarray

---
+++ Arrays/maxSubarray.ts
@@ -0,0 +1,15 @@
+// Given an integer array nums, find the subarray with the largest sum,
+// and return its sum.
+function maxSubArray(nums: number[]): number {
+  let maxVal = nums[0];
+  const dp: number[] = new Array(nums.length).fill(0);
+  dp[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    maxVal = Math.max(maxVal, dp[i]);
+  }
+  return maxVal;
+}
+
+console.log(maxSubArray([-1, -2])); // -1
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
