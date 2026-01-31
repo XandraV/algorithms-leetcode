b31859b91be9405945c0977a914e5619ba38a652
XandraV <veresalexandra95@gmail.com>
2026-01-31 13:04:30 +0000

    clean up

--- DynamicProgramming/longestPalindromeOptimal.ts
+++ DynamicProgramming/longestPalindromeOptimal.ts
@@ -1,17 +1,17 @@
-function longestPalindromeOptimal(s: string): string {
+function longestPalindrome(s: string): string {
   if (s.length === 1) return s;
 
-  let l = 0; // starting index of the longest palindrome found
+  let startIdx = 0; // starting index of the longest palindrome found
   let maxLength = 0; // length of the longest palindrome found
   // Loop through each index as potential center
   for (let i = 0; i < s.length; i++) {
-    // odd
+    // odd case center is s[i]
     let left = i,
       right = i;
     // loop as long as we we stay inside the string and characters match
     while (left >= 0 && right < s.length && s[left] === s[right]) {
       if (right - left + 1 > maxLength) {
-        l = left;
+        startIdx = left;
         maxLength = right - left + 1; // current palindrome length
       }
       // expand
@@ -19,12 +19,12 @@ function longestPalindromeOptimal(s: string): string {
       right++;
     }
 
-    // even
+    // even case center is s[i] and s[i+1]
     left = i;
     right = i + 1;
     while (left >= 0 && right < s.length && s[left] === s[right]) {
       if (right - left + 1 > maxLength) {
-        l = left;
+        startIdx = left;
         maxLength = right - left + 1; // current palindrome length
       }
       left--;
@@ -32,5 +32,7 @@ function longestPalindromeOptimal(s: string): string {
     }
   }
 
-  return s.substring(l, l + maxLength);
+  return s.substring(startIdx, startIdx + maxLength);
 }
+
+console.log(longestPalindrome("cbbd")); // "bb"
