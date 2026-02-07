378c733f9e7be09ea4e716b02cf63d9b93f49862
XandraV <veresalexandra95@gmail.com>
2026-02-07 12:29:44 +0000

    clean up

--- Arrays/topKFrequentNums.ts
+++ Arrays/topKFrequentNums.ts
@@ -1,7 +1,7 @@
 // 347 Top k frequent elements
 // Given an integer array nums and an integer k, return the k most
 // frequent elements. You may return the answer in any order.
-function topKFrequent(nums: number[], k: number): number[] {
+function topKFrequentNums(nums: number[], k: number): number[] {
   const freqMap = new Map<number, number>();
 
   for (let num of nums) {
@@ -14,5 +14,5 @@ function topKFrequent(nums: number[], k: number): number[] {
     .slice(0, k);
 }
 
-console.log(topKFrequent([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], 2));
+console.log(topKFrequentNums([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], 2));
 // [1,2]
