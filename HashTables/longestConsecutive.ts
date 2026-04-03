41b2c8b4770e4f5d55a51a2beb3025bbb3c7aef0
XandraV <veresalexandra95@gmail.com>
2026-04-03 11:09:48 +0100

    longest consecutive

--- HashTables/longestConsecutive.ts
+++ HashTables/longestConsecutive.ts
@@ -1,3 +1,8 @@
+// 128. Longest Consecutive Sequence
+// Given an unsorted array of integers nums, 
+// return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
 function longestConsecutive(nums: number[]): number {
   let longest = 0;
   const numSet = new Set(nums);
