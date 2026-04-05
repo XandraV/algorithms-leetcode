500451113da4fb7050a0912e174c2f47a6825537
XandraV <veresalexandra95@gmail.com>
2026-04-05 13:46:22 +0100

    fibonacci

--- LeetcodeMix/fibonacci.ts
+++ LeetcodeMix/fibonacci.ts
@@ -8,9 +8,9 @@
 // directly return the previously calculated value from it
 // otherwise, call the original function, store the returned value
 // in the cache, and return it.
-const memoize = (fn) => {
-  const cache = {};
-  const memoizedFn = (...args) => {
+const memoize = (fn: (...args: any[]) => any) => {
+  const cache: Record<string, any> = {};
+  const memoizedFn = (...args: any[]) => {
     const key = JSON.stringify(args);
     if (key in cache) return cache[key];
     return (cache[key] = fn(...args));
@@ -20,7 +20,7 @@ const memoize = (fn) => {
 
 // fibonacci plain recursive implementation
 // O(2^n - 1)
-function fibonacci(n) {
+function fibonacci(n: number): number {
   if (n < 2) {
     return n;
   } else {
@@ -28,25 +28,25 @@ function fibonacci(n) {
   }
 }
 // memoized version of fibonacci plain recursive implementation - Top down DP
-const fiboMemoised = memoize((n) => {
+const fiboMemoised = memoize((n: number) => {
   if (n < 2) return n;
   return fiboMemoised(n - 1) + fiboMemoised(n - 2);
 });
 
 // O(n) because of caching only calculating everything once
-function fib(N) {
-  let cache = {};
+function fib(N: number): number {
+  let cache: Record<number, number> = {};
 
-  function recur_fib(N) {
-    let result = null;
-    if (Object.keys(cache).includes(N)) {
+  function recurFib(N: number): number {
+    let result: number | null = null;
+    if (N in cache) {
       return cache[N];
     }
 
     if (N < 2) {
       result = N;
     } else {
-      result = recur_fib(N - 1) + recur_fib(N - 2);
+      result = recurFib(N - 1) + recurFib(N - 2);
     }
     // put result in cache for later reference.
     cache[N] = result;
@@ -54,7 +54,7 @@ function fib(N) {
     return result;
   }
 
-  return recur_fib(N);
+  return recurFib(N);
 }
 console.log(fib(8));
 
@@ -71,13 +71,16 @@ the same way that the series is defined, starting with 0 and 1,
 and always adding the last two numbers to create the next number in 
 the sequence:
 */
-const fiboWhileLoop = (n) => {
+const fiboWhileLoop = (n: number): number => {
   // need no calculations
   if (n < 2) return n;
   // set up a loop with a and b represent the two latest numbers in the sequence,
   // and loop enough times until b becomes the number you are seeking
   let a = 0;
   let b = 1;
+  // a, b -> a is second latest, b is latest
+  // to get the next number in the sequence, we add a and b together
+  // a becomes b, b becomes a+b | a -> b, b-> a+b
   while (n > 1) {
     [a, b] = [b, a + b];
     n--;
@@ -87,5 +90,22 @@ const fiboWhileLoop = (n) => {
 // the previously calculated numbers aren’t saved
 // because the algorithm works in a bottom-up fashion,
 // calculating later numbers by using previous ones;
-// you always need only the two latest numbers,
+// you always need only the two ∏latest numbers,
 // so there’s no need to store all the others.
+
+// Dynamic proramming solution
+const fibDP = (n: number): number => {
+  // next = prev + curr
+  const dp = [];
+
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+};
+
+console.log(fibDP(5)); // 5
