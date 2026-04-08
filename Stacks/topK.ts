53f217fc07eea1291288da0365ee44ab5525826d
XandraV <veresalexandra95@gmail.com>
2026-04-08 11:27:23 +0100

    clean up

--- Stacks/topK.ts
+++ Stacks/topK.ts
@@ -1,5 +1,6 @@
-// Given an unsorted array of integers which might have duplicates, 
+// Given an unsorted array of integers which might have duplicates,
 // return the top k integers in non-ascending order.
+// O(n⋅k)
 function topK(arr: number[], k: number): number[] {
   let stack = [];
   let minIndex = -1;
@@ -7,13 +8,13 @@ function topK(arr: number[], k: number): number[] {
   for (let item of arr) {
     if (stack.length < k) {
       stack.push(item);
-      minIndex = stack.indexOf(Math.min(...stack));
     } else {
       if (item > stack[minIndex]) {
         stack[minIndex] = item;
-        minIndex = stack.indexOf(Math.min(...stack));
       }
     }
+    let minVal = Math.min(...stack);
+    minIndex = stack.indexOf(minVal);
   }
 
   return stack.sort((a, b) => b - a);
@@ -21,5 +22,5 @@ function topK(arr: number[], k: number): number[] {
 
 console.log(topK([1, 10, 8, 9, 10, 2, 3, 4, 8, 8, 6], 4)); // [10,10,9,8]
 console.log(
-  topK([1, 2, 6, 9, 5, 9, 5, 1, 5, 2, 4, 1, 4, 5, 5, 4, 9, 6, 8, 6], 4)
+  topK([1, 2, 6, 9, 5, 9, 5, 1, 5, 2, 4, 1, 4, 5, 5, 4, 9, 6, 8, 6], 4),
 ); // [9,9,9,8]
