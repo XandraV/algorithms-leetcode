eaf425f5edfb2e7a2ebf51f3c07c79ccd1e70613
XandraV <veresalexandra95@gmail.com>
2026-04-12 11:34:45 +0100

    all path from source to target

--- Graphs/allPathsSourceTargetBacktrack.ts
+++ Graphs/allPathsSourceTargetBacktrack.ts
@@ -1,5 +1,4 @@
 // 797. All Paths From Source to Target
-
 // Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1,
 // find all possible paths from node 0 to node n - 1 and return them in any order.
 // The graph is given as follows: graph[i] is a list of all nodes you can visit
