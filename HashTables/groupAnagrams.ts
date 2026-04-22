e2987501ad2659c3f7f5ff6b85b5b065a86f74a9
XandraV <veresalexandra95@gmail.com>
2026-04-22 18:46:27 +0100

    clean up

--- HashTables/groupAnagrams.ts
+++ HashTables/groupAnagrams.ts
@@ -29,37 +29,7 @@ function groupAnagrams1(strs: string[]): string[][] {
   return Array.from(aMap.values());
 }
 
-console.log("Lowercase Anagrams:");
-console.log("Input: ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']");
 console.log(
   "Output: ",
   JSON.stringify(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])),
 );
-
-console.log("Mixed Case Anagrams:");
-console.log("Input: ['Eat', 'Tea', 'Tan', 'Ate', 'Nat', 'Bat']");
-console.log(
-  "Output: ",
-  JSON.stringify(groupAnagrams(["Eat", "Tea", "Tan", "Ate", "Nat", "Bat"])),
-);
-
-console.log("No Anagrams:");
-console.log("Input: ['hello', 'world', 'test']");
-console.log(
-  "Output: ",
-  JSON.stringify(groupAnagrams(["hello", "world", "test"])),
-);
-
-// ---------------
-// Empty Strings
-// ---------------
-console.log("Empty Strings:");
-console.log("Input: ['', '', '']");
-console.log("Output: ", JSON.stringify(groupAnagrams(["", "", ""])));
-
-// ---------------
-// Single Characters
-// ---------------
-console.log("Single Characters:");
-console.log("Input: ['a', 'b', 'a']");
-console.log("Output: ", JSON.stringify(groupAnagrams(["a", "b", "a"])));
